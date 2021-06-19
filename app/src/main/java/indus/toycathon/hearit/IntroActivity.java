d434970639bbebb5a55602a7aa156d56524301b5
Siddharth280301 <siddharthpardeshi280301@gmail.com>
2021-06-19 14:32:47 +0530

    Added Intro and Home Activity.

---
+++ app/src/main/java/indus/toycathon/hearit/IntroActivity.java
@@ -0,0 +1,21 @@
+package indus.toycathon.hearit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Button startBtn = (Button)findViewById(R.id.startBtn);
+        startBtn.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), MainActivity.class);
+            startActivity(intent);
+        });
+    }
+}
--- app/src/main/java/indus/toycathon/hearit/MainActivity.java
+++ app/src/main/java/indus/toycathon/hearit/MainActivity.java
@@ -1,7 +1,9 @@
 package indus.toycathon.hearit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button arBtn = (Button)findViewById(R.id.arBtn);
+        Button lensBtn = (Button)findViewById(R.id.lensBtn);
+
+        // on AR button clicked
+        arBtn.setOnClickListener(view -> {
+            Toast.makeText(getApplicationContext(), "AR button clicked.", Toast.LENGTH_SHORT).show();
+        });
+
+        //on Lens button clicked
+        lensBtn.setOnClickListener(view -> {
+            Toast.makeText(getApplicationContext(), "Lens button clicked.", Toast.LENGTH_SHORT).show();
+        });
     }
 }
